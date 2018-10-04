19b40fde276bf7c880a81a10bc48d250318957e8
GreggX <gregoryglez11@gmail.com>
2018-10-03 23:14:34 -0500

    primer commit

---
+++ src/main/java/com/generation/reactspringrestsecurity/ReactSpringRestSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.generation.reactspringrestsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringRestSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringRestSecurityApplication.class, args);
+	}
+}
